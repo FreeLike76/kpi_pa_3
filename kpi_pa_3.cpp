e7541faacec51df8f5ec6e3e4a431d761509d8df
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-10-19 13:26:03 +0300

    Added template for main cycle and innitial ant spread functions

--- kpi_pa_3.cpp
+++ kpi_pa_3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <cmath>
+#include <ctime>
 #include "Graph.h"
 #include "Ant.h"
 
@@ -8,18 +10,72 @@ using namespace std;
 
 #define defFile "matrix.txt"
 #define defPheromone 1
+#define defMaxIter 1000
 
-#define defA 1
-#define defB 1
+#define defA 0.5
+#define defB 0.5
 #define defRo 0.5
 #define defAnt 50
 #define defLmin 10
 #define defSpread true
 
+void spreadAntsT(vector<Ant>& allAnts, int vertCount);
+void spreadAntsF(vector<Ant>& allAnts, int vertCount);
+
+
 int main()
 {
-	//Choosing a parameter to test
-	auto testParam = defA;
 	Graph graph(defFile,defPheromone);
 	vector<Ant> allAnts(defAnt);
+
+	int Lcur = INFINITY;
+	vector<int> Tpath;
+
+	for (int mainIter = 0; mainIter < defMaxIter; mainIter++)
+	{
+		if(defSpread)
+		{
+			spreadAntsT(allAnts, graph.size());
+		}
+		else
+		{
+			spreadAntsF(allAnts, graph.size());
+		}
+		for (int antIter = 0; antIter < allAnts.size(); antIter++)
+		{
+			vector<int> avalVert(graph.size(), true);
+			avalVert[allAnts[antIter].pathHist.front()] = false;
+			for (int vertIter = 0; vertIter < graph.size(); vertIter++)
+			{
+
+
+
+			}
+		}
+	}
+}
+
+void spreadAntsT(vector<Ant> &allAnts,int vertCount)
+{
+	vector<int> Spread;
+	Spread.reserve(vertCount);
+	for (int i = 0; i < vertCount; i++)
+	{
+		Spread.push_back(i);
+	}
+	random_shuffle(Spread.begin(), Spread.end());
+	for (int i = 0; i < allAnts.size(); i++)
+	{
+		allAnts[i].pathHist.push_back(Spread[i]);
+	}
+	Spread.clear();
+}
+
+void spreadAntsF(vector<Ant>& allAnts, int vertCount)
+{
+	int a = rand() % (vertCount+1);
+	for (int i = 0; i < allAnts.size(); i++)
+	{
+		allAnts[i].pathHist.push_back(a);
+	}
 }
