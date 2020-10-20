2eb6ebcd8860352d203867771230b183922ee4fb
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-10-20 12:42:13 +0300

    Code for ant colony is done. Starting to debug and then rewriting main() for finding best parameters (A, B, Ro, AntCount, Lmin, antSpread T/F).

--- kpi_pa_3.cpp
+++ kpi_pa_3.cpp
@@ -12,21 +12,28 @@ using namespace std;
 #define defPheromone 1
 #define defMaxIter 1000
 
-#define defA 0.5
-#define defB 0.5
-#define defRo 0.5
+#define defA 3
+#define defB 2
+#define defRo 0.3
 #define defAnt 50
 #define defLmin 10
 #define defSpread true
 
-vector<int> doAntCycle(Graph& graph, vector<Ant>& allAnts, double A, double B, double Ro, int Lmin);
+int doAntCycle(Graph& graph, vector<Ant>& allAnts, double A, double B, double Ro, int Lmin);
 
 void spreadAntsT(vector<Ant>& allAnts, int vertCount);
 void spreadAntsF(vector<Ant>& allAnts, int vertCount);
+int evalNextVert(Graph& graph, int curVert, vector<bool>& avalVert, double A, double B);
+void pheromoneDecay(Graph& graph, double Ro);
+void printDetails(Ant& ant);
+void addPheromone(Graph& graph, vector<Ant>& allAnts, int Lmin);
+void antReset(vector<Ant>& allAnts);
+
 
 int main()
 {
-	Graph graph(defFile,defPheromone);
+	srand(time(NULL));
+	Graph graph(defFile, defPheromone);
 	vector<Ant> allAnts(defAnt);
 
 	int Lcur = INFINITY;
@@ -34,12 +41,24 @@ int main()
 
 	for (int mainIter = 0; mainIter < defMaxIter; mainIter++)
 	{
-		auto thisPath = doAntCycle(graph, allAnts, defA, defB, defRo, defLmin);
+		int bestAntID = doAntCycle(graph, allAnts, defA, defB, defRo, defLmin);
+		pheromoneDecay(graph, defRo);
+		addPheromone(graph, allAnts, defLmin);
+		if (mainIter % 50 == 1)
+			printDetails(allAnts[bestAntID]);
+
+		if (Lcur > allAnts[bestAntID].getPathLen())
+		{
+			Lcur = allAnts[bestAntID].getPathLen();
+			Tpath.clear();
+			Tpath = allAnts[bestAntID].pathHist;
+		}
+		antReset(allAnts);
 	}
 }
 
 
-vector<int> doAntCycle(Graph &graph,vector<Ant> &allAnts,double A,double B,double Ro,int Lmin)
+int doAntCycle(Graph &graph,vector<Ant> &allAnts,double A,double B,double Ro,int Lmin)
 {
 	if (defSpread)
 	{
@@ -49,17 +68,29 @@ vector<int> doAntCycle(Graph &graph,vector<Ant> &allAnts,double A,double B,doubl
 	{
 		spreadAntsF(allAnts, graph.size());
 	}
-	for (int antIter = 0; antIter < allAnts.size(); antIter++)
+	for (int antID = 0; antID < allAnts.size(); antID++)
 	{
 		vector<bool> avalVert(graph.size(), true);
-		avalVert[allAnts[antIter].pathHist.front()] = false;
-		for (int vertIter = 0; vertIter < graph.size(); vertIter++)
+		avalVert[allAnts[antID].pathHist.front()] = false;
+		for (int pathIter = 1; pathIter < graph.size(); pathIter++)
 		{
-
+			int nextVert = evalNextVert(graph, allAnts[antID].pathHist.back(), avalVert, A, B);
+			allAnts[antID].pathHist.push_back(nextVert);
+			avalVert[nextVert] = false;
+		}
+		allAnts[antID].calcAntPath(graph);
+	}
+	int minPath = allAnts[0].getPathLen();
+	int minAntID = 0;
+	for (int i = 1; i < allAnts.size(); i++)
+	{
+		if(minPath>allAnts[i].getPathLen())
+		{
+			minPath = allAnts[i].getPathLen();
+			minAntID = i;
 		}
 	}
-	//find min
-	//return vector {pathLen,vert1,vert2,...,vertN}
+	return minAntID;
 }
 
 void spreadAntsT(vector<Ant> &allAnts,int vertCount)
@@ -87,12 +118,65 @@ void spreadAntsF(vector<Ant>& allAnts, int vertCount)
 	}
 }
 
-int evalNextVert(Graph graph,int curVert, vector<bool> avalVert,double A,double B)
+int evalNextVert(Graph &graph,int curVert, vector<bool> &avalVert,double A,double B)
 {
 	double totalPrice=0;
 	for (int i = 0; i < avalVert.size(); i++)
 	{
 		if(avalVert[i])
 			totalPrice += pow(graph.pheromone[curVert][i], A) * pow((double(1) / double(graph.adj[curVert][i])), B);
+	}
+	int requiredChance = rand() % 100;
+	double chance = 0;
+	for (int i = 0; i < avalVert.size(); i++)
+	{
+		if(avalVert[i])
+		{
+			chance += (pow(graph.pheromone[curVert][i], A) * pow((double(1) / double(graph.adj[curVert][i])), B) * 100) / totalPrice;
+			if (chance > requiredChance)
+				return i;
+		}
+	}
+}
+
+void pheromoneDecay(Graph &graph, double Ro)
+{
+	for (int i = 0; i < graph.size(); i++)
+	{
+		for (int j = 0; j < graph.size(); j++)
+		{
+			graph.pheromone[i][j] *= (1 - Ro);
+		}
+	}
+}
+
+void printDetails(Ant& ant)
+{
+	cout << "Min Length: " << ant.getPathLen() << endl << " Path: ";
+	for (int i = 0; i < ant.pathHist.size(); i++)
+	{
+		cout << ant.pathHist[i] << " ";
+	}
+	cout << ant.pathHist[0] << endl;
+}
+
+void addPheromone(Graph &graph, vector<Ant> &allAnts,int Lmin)
+{
+	for (int antID = 0; antID < allAnts.size(); antID++)
+	{
+		double addP = double(Lmin) / double(allAnts[antID].getPathLen());
+		for (int i = 0; i < allAnts[antID].pathHist.size() - 1; i++)
+		{
+			graph.pheromone[allAnts[antID].pathHist[i]][allAnts[antID].pathHist[i + 1]] += addP;
+		}
+		graph.pheromone[allAnts[antID].pathHist.back()][allAnts[antID].pathHist.front()] += addP;
+	}
+}
+
+void antReset(vector<Ant> &allAnts)
+{
+	for (int i = 0; i < allAnts.size(); i++)
+	{
+		allAnts[i].clearMemory();
 	}
 }
