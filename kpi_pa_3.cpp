b912b844d6f99b24b8d95b156457b5e60329f089
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-10-21 16:07:39 +0300

    Starting testing a,b

--- kpi_pa_3.cpp
+++ kpi_pa_3.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <ctime>
 #include <fstream>
+#include <chrono>
 #include "Graph.h"
 #include "Ant.h"
 
@@ -11,36 +12,36 @@ using namespace std;
 
 #define defFile "matrix.txt"
 #define defPheromone 1
-#define defMaxIter 1000
+#define defMaxIter 100
 #define defRequiredLen 1600
-#define test_step 0.1
+#define defStep 1
 
 //_test1 minValue to test
 //_test2 maxValue to test
 //_best best velue, founded in testing
 
-#define defA_test1 0.1 
-#define defA_test2 4
+#define defA_test1 0
+#define defA_test2 10
 #define defA_best 2
 //
-#define defB_test1 0.1
-#define defBt_test2 4
+#define defB_test1 0
+#define defB_test2 10
 #define defB_best 3
 //
-#define defRot_test1 0.1
-#define defRot_test2 0.9
+#define defRo_test1 0.1
+#define defRo_test2 0.9
 #define defRo_best 0.3
 //
-#define defAnt_test1 45
-#define defAntt_test2 45
-#define defAnt_best 45
+#define defAnt_test1 10
+#define defAnt_test2 200
+#define defAnt_best 5
 //
-#define defLmint_test1 1000
-#define defLmint_test2 5000
+#define defLmin_test1 100
+#define defLmin_test2 5000
 #define defLmin_best 1000
 //
-#define defSpread_test1 true
-#define defSpreadt_test2 false
+#define defSpread_test1 false
+#define defSpread_test2 true
 #define defSpread_best true
 
 int findPathUntil(int RequiredPath, int A, int B, int Ro, int Lmin, int AntCount, bool Spread);
@@ -52,49 +53,67 @@ int evalNextVert(Graph& graph, int curVert, vector<bool>& avalVert, double A, do
 void pheromoneDecay(Graph& graph, double Ro);
 void addPheromone(Graph& graph, vector<Ant>& allAnts, int Lmin);
 void antReset(vector<Ant>& allAnts);
-
-template<class T1, class T2>
-void csvWriter(string path, pair<T1, T2> res);
+void csvWriter(string path, double testParam, pair<int, float> details);
 
 int main()
 {
-	string test_param_name = "A_test.csv";
+	string testOutputFile = "A";
 	srand(time(NULL));
-	for (auto testParam = defA_test1; testParam < defA_test2; testParam += test_step)
+	for (auto testParam = defA_test1; testParam < defA_test2; testParam += defStep)
 	{
-		int result = findPathUntil(defRequiredLen,defA_best, defB_best, defRo_best, defLmin_best, defAnt_best, defSpread_best);
-		csvWriter(test_param_name, make_pair(testParam, result));
+		auto start = chrono::high_resolution_clock::now();
+		int tookIter = findPathUntil(defRequiredLen, testParam, defB_best, defRo_best, defLmin_best, defAnt_best, defSpread_best);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<float> tookTime = end - start;
+		csvWriter(testOutputFile, testParam,make_pair(tookIter,tookTime.count()));
+	}
+	testOutputFile = "B";
+	for (auto testParam = defB_test1; testParam < defB_test2; testParam += defStep)
+	{
+		auto start = chrono::high_resolution_clock::now();
+		int tookIter = findPathUntil(defRequiredLen, testParam, defB_best, defRo_best, defLmin_best, defAnt_best, defSpread_best);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<float> tookTime = end - start;
+		csvWriter(testOutputFile, testParam, make_pair(tookIter, tookTime.count()));
 	}
 }
 
 
 int findPathUntil(int RequiredPath, int A, int B, int Ro, int Lmin, int AntCount, bool Spread)
+{
+	auto start = chrono::high_resolution_clock::now();
+	Graph graph(defFile, defPheromone);
+	vector<Ant> allAnts(AntCount);
+
+	int Lcur;
+	//vector<int> Tpath;
+	int lock_counter = 0;
+
+	for (int mainIter = 0; mainIter < defMaxIter; mainIter++)
 	{
-		Graph graph(defFile, defPheromone);
-		vector<Ant> allAnts(AntCount);
-
-		int Lcur;
-		vector<int> Tpath;
-
-		int requiredIter = 0;
-
-		for (int mainIter = 0; mainIter < defMaxIter; mainIter++)
+		int bestAntID = doAntCycle(graph, allAnts, A, B, Ro, Lmin, Spread);
+		pheromoneDecay(graph, Ro);
+		addPheromone(graph, allAnts, Lmin);
+		if (mainIter == 0 || Lcur > allAnts[bestAntID].getPathLen())
 		{
-			int bestAntID = doAntCycle(graph, allAnts, A, B, Ro, Lmin, Spread);
-			pheromoneDecay(graph, Ro);
-			addPheromone(graph, allAnts, Lmin);
-
-			if (mainIter == 0 || Lcur > allAnts[bestAntID].getPathLen())
-			{
-				Lcur = allAnts[bestAntID].getPathLen();
-				Tpath.clear();
-				Tpath = allAnts[bestAntID].pathHist;
-			}
-			cout << "Min Path Len: " << Lcur << endl;
-			antReset(allAnts);
+			Lcur = allAnts[bestAntID].getPathLen();
+			/*Tpath.clear();
+			Tpath = allAnts[bestAntID].pathHist;*/
+			lock_counter = 0;
 		}
-		return requiredIter;
+		else
+			lock_counter++;
+		if (lock_counter > 20)
+			break;
+		cout << "Min Path Len: " << Lcur << endl;
+		antReset(allAnts);
+		if(Lcur<defRequiredLen)
+		{
+			return mainIter;
+		}
 	}
+	return defMaxIter;
+}
 
 int doAntCycle(Graph & graph, vector<Ant> & allAnts, double A, double B, double Ro, int Lmin, bool Spread)
 	{
@@ -209,11 +228,16 @@ void antReset(vector<Ant> & allAnts)
 		}
 	}
 
-template<class T1, class T2>
-void csvWriter(string path, pair<T1, T2> res)
-	{
-		ofsteam a;
-		a.open("csvResults\\" + path, ios::app);
-		a << res.first() << '\t' << res.second() << endl;
-		a.close();
-	}
+
+void csvWriter(string path, double testParam, pair<int, float> details)
+{
+	cout << testParam << '\t' << details.first << '\t' << details.second << endl;
+	fstream a;
+	a.open("csvResults\\" + path + "_iter.csv", ios::app);
+	a << testParam << '\t' << details.first << endl;
+	a.close();
+
+	a.open("csvResults\\" + path + "_time.csv", ios::app);
+	a << testParam << '\t' << details.second << endl;
+	a.close();
+}
